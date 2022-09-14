afcc1e34a230dbe02252ac274234cb5b88c60080
Ignacio Viqueira <113035210+ignacioviqueirag@users.noreply.github.com>
2022-09-14 11:42:24 -0300

    Add files via upload

---
+++ proyects/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ignacio.viqueira/Desktop/proyects
+# Build directory: C:/Users/ignacio.viqueira/Desktop/proyects/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
